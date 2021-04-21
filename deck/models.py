ddefc031b0a1aa163c74d563f96accfc699f38e7
lorenzosaraiva <lorenzo.saraiva@hotmail.com>
2021-04-20 22:16:16 -0300

    ✨ Adds hands cycle
    The hand now ends and another one starts. The winner is not calculated yet, so the pot goes to a arbritary player.

--- deck/models.py
+++ deck/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=3)
+    binary_position = models.CharField(max_length=5)
+
+# O Model precisa ter só o que front end lê?
+
+		
--- deck/views.py
+++ deck/views.py
@@ -28,22 +28,22 @@ class Player():
 		self.stack = 1000
 
 class Table():
-	def __init__(self, flop1, flop2, flop3, total_players, players, deck):
+	def __init__(self, flop1, flop2, flop3, players, deck):
 		self.flop1 = flop1
 		self.flop2 = flop2
 		self.flop3 = flop3
 		self.turn = "Error"
 		self.river = "Error"
-		self.total_players = total_players
-		self.active_players = total_players
+		self.all_players = players
+		self.active_players = len(players)
 		self.checked_players = 0
 		self.players = players
 		self.current_player = 0
 		self.game_phases = [0, 1, 2, 3, 4] # 0 - pre-flop, 1 - flop, 2 - turn, 3 - river.
-		self.current_phase = 0
+		self.phase = 0
 		self.deck = deck
 		self.finished = 0
-		self.current_pot = 0
+		self.pot = 0
 		self.to_pay = 0
 	
 	def check(self):
@@ -54,7 +54,6 @@ class Table():
 			response = "Player " + str(self.current_player) + " has checked."
 			if self.checked_players == self.active_players: # acabou rodada de apostas
 				self.next_phase()
-				self.next_player()
 			else: #não acabou a rodada de apostas, passa pro proximo player
 				self.next_player()
 		else:
@@ -73,7 +72,7 @@ class Table():
 		player = self.players[self.current_player]
 		total = amount + self.to_pay
 		if player.stack >= amount + self.to_pay:
-			self.current_pot = self.current_pot + total
+			self.pot = self.pot + total
 			self.checked_players = 1
 			self.to_pay = total
 			player.current_bet = player.current_bet + total
@@ -83,46 +82,73 @@ class Table():
 		# player sem dinheiro
 
 	def call (self):
+		if self.to_pay == 0:
+			return "Nothing to call, either check or raise."
 		player = self.players[self.current_player]
 		response = "Player " + str(self.current_player) + " "
-		if player.stack >= self.to_pay:
-			self.current_pot = self.current_pot + self.to_pay
-			player.stack = player.stack - self.to_pay
-			player.current_bet = self.to_pay
+		total = self.to_pay - player.current_bet
+		if player.stack >= total:
+			self.pot = self.pot + total
+			player.stack = player.stack - total
+			player.current_bet = player.current_bet + self.to_pay
 			self.checked_players = self.checked_players + 1
 			if self.checked_players == self.active_players: # acabou rodada de apostas
 				self.next_phase()
-			self.next_player()
+			else:
+				self.next_player()
 			response = response + "has called for " + str(self.to_pay) + " chips."
 			return response
 		else:
 			response = response + "has not enough to cover, so they went all in. NOT IMPLEMENTED YET."
-			return 
+			return response
 
 	def fold (self):
 		del self.players[self.current_player]
 		self.active_players = self.active_players - 1
 		if self.active_players == 1:
 			#acabou mão, player ganhou
-			finish_hand(self)
+			self.finish_hand()
 
 	def finish_hand(self):
-		print("a")
+		# calcular vencedor. pegando sempre o mesmo por enquantop
+		winner = 0
+		player = self.players[winner]
+		player.stack = player.stack + self.pot
+		self.pot = 0
+		self.phase = 0 
+		self.current_player = 0
+		self.active_players = len(self.all_players)
+		self.checked_players = 0
+		self.to_pay = 0
+		self.players = []
+		self.deck = build_deck()
+		for player in self.all_players:
+			player.card1 = [Card(compute_draw_card(self.deck), 0)]
+			player.card2 = [Card(compute_draw_card(self.deck), 0)]
+		self.flop1 = compute_draw_card(self.deck)
+		self.flop2 = compute_draw_card(self.deck)
+		self.flop3 = compute_draw_card(self.deck)
+		self.players = self.all_players
 		# dar o dinheiro pro player vencedor e resetar tudo.
 
 	def next_phase(self):        
-		self.current_phase = self.current_phase + 1
+		self.phase = self.phase + 1
 		self.checked_players = 0
 		self.to_pay = 0
-		if self.current_phase == 1:
+		self.current_player = 0
+
+		for player in self.players:
+			player.current_bet = 0
+
+		if self.phase == 1:
 			print("FLOP")
 			print("Table:")
 			print(self.flop1, end=' ')
 			print(self.flop2, end=' ')
 			print(self.flop3)
 
-		if self.current_phase == 2:
-			self.turn = draw_card(self.deck)
+		if self.phase == 2:
+			self.turn = compute_draw_card(self.deck)
 			print("TURN")
 			print("Table")
 			print(self.flop1, end=' ')
@@ -130,8 +156,8 @@ class Table():
 			print(self.flop3, end=' ')
 			print(self.turn)
 
-		if self.current_phase == 3:
-			self.river = draw_card(self.deck)
+		if self.phase == 3:
+			self.river = compute_draw_card(self.deck)
 			print("RIVER")
 			print("Table")
 			print(self.flop1, end=' ')
@@ -140,8 +166,8 @@ class Table():
 			print(self.turn, end=' ')
 			print(self.river)
 
-		if self.current_phase == 4:
-			self.finish_hand(self)
+		if self.phase == 4:
+			self.finish_hand()
 
 
 def build_deck():
@@ -166,8 +192,7 @@ def view_deck(request):
 	return HttpResponse(cache.get('deck'))
 
 def compute_draw_card(deck):
-	if deck == None:
-		deck = build_deck()
+	deck = cache.get('deck', deck)
 	position = random.randint(0, len(deck) - 1)
 	card = deck.pop(position)
 	cache.set('deck', deck)
@@ -180,32 +205,38 @@ def show_table(request):
 	table = cache.get('table')
 	response = "" 
 	i = 0
-	if table.current_phase >= 1:
+	if table.phase >= 1:
 		response = response + str(table.flop1) + str(table.flop2) + str(table.flop3)
 
-	if table.current_phase >= 2:
+	if table.phase >= 2:
 		response = response + str(table.turn)
 
-	if table.current_phase == 3:
+	if table.phase == 3:
 		response = response + str(table.river)
 
-	response = response + " Pot: " + str(table.current_pot) + " Active player is " + str(table.current_player) + ".\n"
+	response = response + " Pot: " + str(table.pot) + " Active player is " + str(table.current_player) + ".\n"
 
 	for player in table.players:
-		response = response + " Player " + str(i) + " has " + str(player.stack) + " chips. \n"
+		response = response + " Player " + str(i) + " has " + str(player.stack) + " chips and "
+		for card in player.card1:
+			response = response + card.name
+		for card in player.card2:
+			response = response + card.name
+		response = response + " as hand."
 		i = i + 1
 	return HttpResponse(response)
 
 def build_hand(request):
-	deck = cache.get('deck')
-
+	deck = build_deck()
+	
 	player1 = Player(Card(compute_draw_card(deck),0), Card(compute_draw_card(deck),0), cirq.LineQubit.range(10), 1, cirq.Circuit())
 	player2 = Player(Card(compute_draw_card(deck),0), Card(compute_draw_card(deck),0), cirq.LineQubit.range(10), 2, cirq.Circuit())
 
-	table = Table(compute_draw_card(deck), compute_draw_card(deck), compute_draw_card(deck), 2, [player1, player2], deck)
+	table = Table(compute_draw_card(deck), compute_draw_card(deck), compute_draw_card(deck), [player1, player2], deck)
 	cache.set('table', table)
+
 	
-	#response = str(table.flop1) + str(table.flop2) + str(table.flop3) + str(table.current_pot)
+	#response = str(table.flop1) + str(table.flop2) + str(table.flop3) + str(table.pot)
 	return HttpResponse("Hand Start!")
 
 def check(request):
