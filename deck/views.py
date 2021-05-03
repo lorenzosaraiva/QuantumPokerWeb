99a75f002d25e570cecc858d4d83d97711d4b371
lorenzosaraiva <lorenzo.saraiva@hotmail.com>
2021-05-02 22:18:30 -0300

    🚧 Basic HTML with buttons added

--- deck/views.py
+++ deck/views.py
@@ -10,6 +10,9 @@ from django.core.cache import cache
 from cirq import Simulator
 from pokereval.card import Card
 from pokereval.hand_evaluator import HandEvaluator
+from django.template import loader
+from django.template.loader import render_to_string
+from django.views.decorators.csrf import csrf_exempt
 
 
 class _Card():
@@ -34,6 +37,17 @@ class Player():
 		self.current_bet = 0
 		self.stack = 1000
 
+	def get_card1(self):
+		ret = ""
+		for card in self.card1:
+			ret = ret + str(card.name)
+		return ret
+
+	def get_card2(self):
+		ret = ""
+		for card in self.card2:
+			ret = ret + str(card.name)
+		return ret
 
 class Table():
 	def __init__(self, flop1, flop2, flop3, players, deck):
@@ -43,7 +57,7 @@ class Table():
 		self.turn = "Error"
 		self.river = "Error"
 		self.cards = [flop1, flop2, flop3]
-		self.all_players = players
+		self.all_players = players[:]
 		self.active_players = len(players)
 		self.checked_players = 0
 		self.players = players
@@ -55,7 +69,7 @@ class Table():
 		self.pot = 0
 		self.to_pay = 0
 
-	def check(self):
+	def check (self):
 		player = self.players[self.current_player]
 		response = ""
 		if player.current_bet == self.to_pay:  # checa se player já cobriu aposta
@@ -120,7 +134,6 @@ class Table():
 			response_data['phase'] = self.phase
 			return response_data
 
-
 	def raise_bet(self, amount):
 		player = self.players[self.current_player]
 		response_data = {}
@@ -131,9 +144,11 @@ class Table():
 			self.to_pay = amount + self.to_pay 
 			player.current_bet = self.to_pay
 			player.stack = player.stack - total
+			ret = "Player " + str(self.current_player) + "has raised to " + str(self.to_pay)
 			self.next_player()
-		# else:
-		# player sem dinheiro
+			return ret
+		else:
+			return "Not enough money"
 
 
 	def call_JSON(self):
@@ -208,10 +223,23 @@ class Table():
 			self.finish_hand()
 
 	def finish_hand(self):
-		# calcular vencedor. pegando sempre o mesmo por enquanto
-		winner = 0
-		player = self.players[winner]
-		player.stack = player.stack + self.pot
+		winner = ""
+
+		if self.active_players > 1:
+			self.compute_players()
+			score = 0
+			board = [Card(self.flop1.power, self.flop1.suit), Card(self.flop2.power, self.flop2.suit) , Card(self.flop3.power, self.flop3.suit), Card(self.turn.power, self.turn.suit), Card(self.river.power, self.river.suit)]
+			for player in self.players:
+
+				hole = [Card(player.card1[0].power, player.card1[0].suit), Card(player.card2[0].power, player.card2[0].suit)]
+				new_score = HandEvaluator.evaluate_hand(hole, board)
+				if new_score > score:
+					score = new_score
+					winner = player
+		else:
+			winner = self.players[0]
+
+		winner.stack = winner.stack + self.pot
 		self.pot = 0
 		self.phase = 0
 		self.current_player = 0
@@ -226,8 +254,45 @@ class Table():
 		self.flop1 = compute_draw_card(self.deck)
 		self.flop2 = compute_draw_card(self.deck)
 		self.flop3 = compute_draw_card(self.deck)
-		self.players = self.all_players
+		self.players = self.all_players[:]
 		# dar o dinheiro pro player vencedor e resetar tudo.
+
+	def measure_players(self):
+		for player in self.players:
+			for i in range(player.next_qubit1):
+				player.circuit.append(cirq.measure(player.qubits[i]))
+
+			for i in range(player.next_qubit2):
+				player.circuit.append(cirq.measure(player.qubits[i + 5]))
+
+
+	def compute_players(self):
+		
+		self.measure_players()
+		simulator = Simulator()
+		for player in self.players:
+			result = ''
+			if player.next_qubit1 != 0 or player.next_qubit2 != 0:
+				result = simulator.run(player.circuit)
+			res = str(result)
+			bits1 = ''
+			bits2 = ''
+			bit = ''
+			for i in range(len(res)):
+				if res[i] == '=':
+					bit = bit + str(res[i + 1])
+
+			if player.next_qubit1 > 0:
+				bits1 = bit[:player.next_qubit1]
+			if player.next_qubit2 > 0:
+				bits2 = bit[player.next_qubit1:]
+
+		if len(player.card1) > 1:
+			player.card1 = [player.card1.pop(int(bits1, 2))]
+
+		if len(player.card2) > 1:
+			player.card2 = [player.card2.pop(int(bits2, 2))]
+
 
 	def get_active_player(self):
 		return self.players[self.current_player]
@@ -243,36 +308,18 @@ class Table():
 
 		if self.phase == 1:
 			print("FLOP")
-			print("Table:")
-			print(self.flop1, end=' ')
-			print(self.flop2, end=' ')
-			print(self.flop3)
 
 		if self.phase == 2:
 			self.turn = compute_draw_card(self.deck)
 			self.cards.append(self.turn)
-			print("TURN")
-			print("Table")
-			print(self.flop1, end=' ')
-			print(self.flop2, end=' ')
-			print(self.flop3, end=' ')
-			print(self.turn)
 
 		if self.phase == 3:
 			self.river = compute_draw_card(self.deck)
 			self.cards.append(self.river)
 
-			print("RIVER")
-			print("Table")
-			print(self.flop1, end=' ')
-			print(self.flop2, end=' ')
-			print(self.flop3, end=' ')
-			print(self.turn, end=' ')
-			print(self.river)
 
 		if self.phase == 4:
-			compute_players(self)
-			# self.finish_hand()
+			self.finish_hand()
 
 
 def build_deck():
@@ -312,8 +359,36 @@ def compute_draw_card(deck):
 def index(request):
 	return HttpResponse(build_deck())
 
+def show_table_HTML (request, context={}):
+	table = cache.get('table')
+	i = 0
+	if table.phase >= 1:
+		context["flop1"] = table.flop1.name
+		context["flop2"] = table.flop2.name
+		context["flop3"] = table.flop3.name
 
-def show_table(request):
+	if table.phase >= 2:
+		context["turn"] = table.turn.name
+
+	if table.phase == 3:
+		context["river"] = table.river.name
+
+	context["active_player"] = str(table.current_player)
+	context["pot"] = str(table.pot)
+	context["player0_stack"] = str(table.players[0].stack)
+	context["player0_card1"] = table.players[0].get_card1()
+	context["player0_card2"] = table.players[0].get_card2()
+
+	context["player1_stack"] = str(table.players[1].stack)
+	context["player1_card1"] = table.players[1].get_card1()
+	context["player1_card2"] = table.players[1].get_card2()
+	
+	rendered = render_to_string('main.html', context)
+
+	return HttpResponse(rendered)
+
+
+def show_table (request):
 	table = cache.get('table')
 	response = ""
 	i = 0
@@ -324,7 +399,7 @@ def show_table(request):
 	if table.phase >= 2:
 		response = response + str(table.turn.name)
 
-	if table.phase == 3:
+	if table.phase >= 3:
 		response = response + str(table.river.name)
 
 	response = response + " Pot: " + \
@@ -378,6 +453,14 @@ def build_hand_JSON(request):
 
 def check(request):
 	table = cache.get('table')
+	log = table.check()
+	context = {}
+	context["log"] = log
+	cache.set('table', table)
+	return show_table_HTML(request, context)
+
+def check_old(request):
+	table = cache.get('table')
 	response = table.check()
 	cache.set('table', table)
 	return HttpResponse(response)
@@ -392,7 +475,7 @@ def call(request):
 	table = cache.get('table')
 	table.call()
 	cache.set('table', table)
-	return HttpResponse("")
+	return show_table_HTML(request)
 
 def call_JSON(request):
 	table = cache.get('table')
@@ -400,12 +483,21 @@ def call_JSON(request):
 	cache.set('table', table)
 	return HttpResponse(json.dumps(response_data), content_type="application/json")
 
-
-def raise_bet(request, amount=100):
+def fold(request):
 	table = cache.get('table')
-	table.raise_bet(amount)
+	response_data = table.fold()
 	cache.set('table', table)
-	return HttpResponse("Player raised")
+	return show_table_HTML(request)
+
+@csrf_exempt 
+def raise_bet (request):
+	table = cache.get('table')
+	amount = int(request.POST.get('bet'))
+	log = table.raise_bet(amount)
+	context = {}
+	context["log"] = log
+	cache.set('table', table)
+	return show_table_HTML(request, context)
 
 def raise_JSON(request, amount=100):
 	table = cache.get('table')
@@ -415,8 +507,12 @@ def raise_JSON(request, amount=100):
 
 
 
-def draw_card(request):
-	return HttpResponse(compute_draw_card(cache.get('deck')))
+def draw_card (request):
+	#template = loader.get_template('main.html')
+	rendered = render_to_string('main.html', {'current_player': 'Player1', 'stack':10000})
+
+	#return render(request,'main.html')
+	return HttpResponse(rendered)
 
 
 def quantum_draw(request):
@@ -453,7 +549,9 @@ def quantum_draw(request):
 	player.card1 = card
 	cache.set('table', table)
 
-	return HttpResponse("Player has quantum drawed.")
+	context = {}
+	context["log"] = "Player " + str(table.current_player) + " has quantum drawed."
+	return show_table_HTML(request, context)
 
 
 def resolve_player(player, bits1, bits2):
@@ -462,41 +560,6 @@ def resolve_player(player, bits1, bits2):
 
 	if len(player.card2) > 1:
 		player.card2 = [player.card2.pop(int(bits2, 2))]
-
-
-def measure_player(player):
-	for i in range(player.next_qubit1):
-		player.circuit.append(cirq.measure(player.qubits[i]))
-
-	for i in range(player.next_qubit2):
-		player.circuit.append(cirq.measure(player.qubits[i + 5]))
-
-
-def compute_players(table):
-	for player in table.players:
-		measure_player(player)
-
-	simulator = Simulator()
-	for player in table.players:
-		result = ''
-		if player.next_qubit1 != 0 or player.next_qubit2 != 0:
-			result = simulator.run(player.circuit)
-		res = str(result)
-		bits1 = ''
-		bits2 = ''
-		bit = ''
-		for i in range(len(res)):
-			if res[i] == '=':
-				bit = bit + str(res[i + 1])
-
-		if player.next_qubit1 > 0:
-			bits1 = bit[:player.next_qubit1]
-		if player.next_qubit2 > 0:
-			bits2 = bit[player.next_qubit1:]
-
-		resolve_player(player, bits1, bits2)
-
-	cache.set('table', table)
 
 
 def calculate_hand(request):
